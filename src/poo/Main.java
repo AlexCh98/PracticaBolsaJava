d34c19a81d31d94fc78217d48d5eb0387b840699
AlexCh98 <alexcheca.98@gmail.com>
2017-11-29 14:28:44 +0100

    MainAlex

--- src/poo/Main.java
+++ src/poo/Main.java
@@ -1,10 +1,18 @@
 package poo;
 
+import poo.bolsa.BolsaDeValores;
+import poo.bolsa.Empresa;
 import poo.general.InterfazDeUsuario;
 
 public class Main {
 
     public static void main(String[] args) {
         InterfazDeUsuario interfaz = new InterfazDeUsuario();
+        Empresa repsol = new Empresa("Repsol",1.02);
+        BolsaDeValores bolsa = new BolsaDeValores("IBEX");
+        bolsa.añadirEmpresa(repsol);
+        bolsa.imprimirEmpresas();
+        bolsa.actualizarValoresAcciones();
+        bolsa.imprimirEmpresas();
     }
 }
