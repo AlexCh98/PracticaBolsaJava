cff1df49de34e9cdf63397b2d6e4bb4b146b0971
AlexCh98 <alexcheca.98@gmail.com>
2017-11-29 14:27:54 +0100

    BolsaValores

--- src/poo/bolsa/BolsaDeValores.java
+++ src/poo/bolsa/BolsaDeValores.java
@@ -1,4 +1,47 @@
 package poo.bolsa;
 
+import java.util.ArrayList;
+
 public class BolsaDeValores {
+    public String nombreBolsa;
+    public ArrayList<Empresa> listaEmpresas;
+
+    public BolsaDeValores(String nombre) {
+        this.nombreBolsa = nombre;
+        this.listaEmpresas = new ArrayList<>();
+    }
+
+    public void añadirEmpresa(Empresa empresa){
+        this.listaEmpresas.add(empresa);
+    }
+
+    public void eliminarEmpresa (Empresa empresa){
+        this.listaEmpresas.remove(empresa);
+    }
+
+    public void imprimirEmpresas(){
+        ArrayList<Empresa> empresas=this.listaEmpresas;
+        for (Empresa empresa: empresas){
+            System.out.println(empresa.toString());
+        }
+    }
+
+    public void realizarCopiaSeguridad(){
+        //HACER COPIA DE SEGURIDAD
+    }
+
+    public void restaurarCopiaSeguridad(){
+        //HACER COPIA DE SEGURIDAD
+    }
+
+    public StringBuffer realizarOperacion (StringBuffer mensaje){
+
+        return mensaje;
+    }
+    public void actualizarValoresAcciones(){
+        ArrayList<Empresa> empresas=this.listaEmpresas;
+        for (Empresa empresa: empresas){
+            empresa.valorActualEmpresa(Math.random());
+        }
+    }
 }
