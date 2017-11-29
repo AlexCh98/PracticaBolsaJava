3ae5170f2ac3c5aa777f87b4399bff6f38c5e29d
AlexCh98 <alexcheca.98@gmail.com>
2017-11-29 13:32:33 +0100

    EmpresaAlex

--- src/poo/bolsa/Empresa.java
+++ src/poo/bolsa/Empresa.java
@@ -1,4 +1,25 @@
 package poo.bolsa;
 
 public class Empresa {
+    private String nombreEmpresa;
+    private double valorActual;
+    private double valorPrevio;
+
+    public Empresa(String nombreEmpresa) {
+        this.nombreEmpresa = nombreEmpresa;
+    }
+
+    private void calcularValorPrevio (double valor){
+         this.valorPrevio=valor;
+    }
+
+    public void valorActualEmpresa (double valor){ //Actualiza la accion actual y modifica la accion anterior.
+        calcularValorPrevio(valor);
+        this.valorActual=valor;
+    }
+
+    public double diferenciaAcciones (){
+        double diferencia=Math.abs(this.valorActual-this.valorPrevio);
+        return diferencia;
+    }
 }
