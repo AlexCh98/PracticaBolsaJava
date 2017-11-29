6f8e8fce380eb1ee672b2984ed1d1565d84f7aa0
AlexCh98 <alexcheca.98@gmail.com>
2017-11-29 14:28:25 +0100

    EmpresaAlex

--- src/poo/bolsa/Empresa.java
+++ src/poo/bolsa/Empresa.java
@@ -1,12 +1,19 @@
 package poo.bolsa;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
 public class Empresa {
     private String nombreEmpresa;
     private double valorActual;
     private double valorPrevio;
 
-    public Empresa(String nombreEmpresa) {
+    public Empresa(String nombreEmpresa, double valor) {
         this.nombreEmpresa = nombreEmpresa;
+        this.valorPrevio=valor;
+        this.valorActual=valor;
     }
 
     private void calcularValorPrevio (double valor){
@@ -14,12 +21,22 @@ public class Empresa {
     }
 
     public void valorActualEmpresa (double valor){ //Actualiza la accion actual y modifica la accion anterior.
-        calcularValorPrevio(valor);
+        calcularValorPrevio(this.valorActual);
         this.valorActual=valor;
     }
 
     public double diferenciaAcciones (){
         double diferencia=Math.abs(this.valorActual-this.valorPrevio);
         return diferencia;
+    }
+
+    public String toString (){
+        return (new StringBuilder("Nombre: " + this.nombreEmpresa  + ", Valor Actual " + this.valorActual+", Valor Previo "+this.valorPrevio).toString());
+    }
+
+    private void serializar (Empresa empresa,FileOutputStream file) throws IOException {
+        ObjectOutputStream oos = new ObjectOutputStream(file);
+        oos.writeObject(empresa);
+        oos.close();
     }
 }
